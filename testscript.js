00024d8d6dd449e06250be096969a0cb115f2d42
Z-M-Man <112212530+Z-M-Man@users.noreply.github.com>
2024-02-26 16:06:14 -0500

    More javascript

--- testscript.js
+++ testscript.js
@@ -1,9 +1,24 @@
 myitem = document.getElementById("firsttest"); //Finds an html element with id "firsttest" and assigns it to variable "myitem"
+thebutton = document.getElementById("thebutton");
+otheritem = document.getElementById("buttontest");
+textentry = document.getElementById("myinput");
 
 myitem.addEventListener("click", onClick); //Waits for the event "click" to happen and runs the function onClick when it occurs
+thebutton.addEventListener("click", onButtonClick);
+textentry.addEventListener("change", onChange);
 
 //defines the function that will run when myitem is clicked on 
 function onClick() {
     myitem.style.color = "green"; //sets the style color of the element to green
-    myitem.style.fontSize = "26px"
+    myitem.style.fontSize = "26px"; //sets the font size to 26 pixels
+}
+
+function onButtonClick() {
+    otheritem.style.color = "green";
+    otheritem.style.fontSize = "26px";
+}
+
+function onChange() {
+    newtext = myinput.value;
+    otheritem.innerHTML = newtext;
 }
